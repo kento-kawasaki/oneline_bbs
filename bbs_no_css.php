047592add07f7d2de7fc51cdccdd1e4bf16526e5
kento-kawasaki <13e0222@gmail.com>
2016-10-13 10:51:54 +0800

    一覧表示機能を追加

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -12,14 +12,41 @@
     $nickname = $_POST['nickname'];
     $comment = $_POST['comment'];
 
+
     $sql = "INSERT INTO `posts`(`id`, `nickname`, `comment`, `created`) VALUES (null,'".$nickname."','".$comment."',now())";
     $stmt = $dbh->prepare($sql);
     $stmt->execute();
 }
+    
+    //$sql文の作成（select文）
+    $sql ='SELECT * FROM `posts` ORDER BY `created` DESC';
+
+    //SELECT文実行
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    //変数にDBから取得したデータを格納
+
+    //格納する変数の初期化
+    $posts = array();
+
+    //繰り返し文でデータを取得
+    while(1){
+      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+      if($rec == false){
+        //データを最後まで取得したので終了
+        break;
+      }
 
 
 
-$dbh = null;
+
+      //取得したデータを配列に格納しておく
+      $posts[]=$rec;
+
+    }
+
+      //3.DBの切断
+      $dbh = null;
 
 ?>
 <!DOCTYPE html>
@@ -35,6 +62,35 @@ $dbh = null;
       <p><button type="submit" >つぶやく</button></p>
     </form>
     <!-- ここにニックネーム、つぶやいた内容、日付を表示する -->
+
+    <!-- <ul>
+      <li><?php //echo $posts[0]['nickname']; ?> comment 2016/10/13</li>
+      <li>testname 一言つぶやき 2016/10/12</li>
+      <li>テスト太郎 コメント 2016/10/11</li>
+    </ul> --> 
+
+    <ul>
+        <?php 
+            foreach ($posts as $post_each) {
+                echo '<li>';
+                echo $post_each['nickname'].' ';
+                echo $post_each['comment'].' ';
+                
+                //一旦日付型に変換
+                $created = strtotime($post_each['created']);
+
+                //書式を変換
+                $created = date('Y/m/d',$created);
+
+                //echo $post_each['created'].' ';
+                echo $created;
+                echo '</li>';
+              # code...
+            }
+          ?>
+    </ul>
+
+
 
 </body>
 </html>
